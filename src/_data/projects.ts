b2b9efb2b27fffef04aafe1a0c81ea7e8365e383
flpputsch <felipeputsch@unifebe.edu.br>
2018-10-23 10:55:22 -0300

    Adicionado novo projeto

--- src/_data/projects.ts
+++ src/_data/projects.ts
@@ -10,5 +10,13 @@ export const ProjectIdeas : ProjectIdea[] = [
         description: "Help people see their ideais developed by exposing them in an environment of people willing to contribute to open source powered by an website and a github repository",
         recommendedLanguages: [Language.Javascript],
         url: "https://github.com/aletaschner/HacktoberFest2018-ProjectIdeas"
+    },
+     {
+        title : "Indicative of dangerous locations in the city",
+        author: {name: "Felipe Putsch", username: "flpputsch"},
+        categories: [Category.Website],
+        description: "Create a page for indicative the most dangerous locations on the city using police data.",
+        recommendedLanguages: [Language.Javascript],
+        url: ""
     }
 ];
